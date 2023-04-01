f62392b4773fda33ce903b097ec747ff88a7d6b2
Dinu Ionut-Alin <dinui.alin@gmail.com>
2023-04-02 00:21:04 +0300

    [HOTFIX] Bug fix

--- pitcher/src/app/features/category-page/category-page.component.ts
+++ pitcher/src/app/features/category-page/category-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { DataService } from 'src/app/core/services/data.service';
-import { listData } from 'src/app/shared/list';
 import { Category } from 'src/app/shared/models/category';
 @Component({
   selector: 'app-category-page',
@@ -9,7 +7,6 @@ import { Category } from 'src/app/shared/models/category';
 })
 export class CategoryPageComponent implements OnInit {
 
-  list = listData.reverse();
   @ViewChild('searchbar') searchbar!: ElementRef;
   searchText = '';
 
