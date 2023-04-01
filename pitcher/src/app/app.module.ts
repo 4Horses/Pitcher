45ed71c311210e3c22f76009d5248f4edb3e090d
Dinu Ionut-Alin <dinui.alin@gmail.com>
2023-04-01 13:26:27 +0300

    Add basic components for registration

--- pitcher/src/app/app.module.ts
+++ pitcher/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './features/registration/components/login/login.component';
+import { RegisterComponent } from './features/registration/components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
