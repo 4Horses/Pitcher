767249ff8511762cc9ddb724bd38c246de631223
Arhip Florin <65599211+Florin9925@users.noreply.github.com>
2023-04-02 00:20:18 +0300

    Added user account endpoints
    Get all, get by id, patch user

--- pitcher-be/src/main/java/com/fourhorses/pitcherbe/user_account/controller/UserAccountController.java
+++ pitcher-be/src/main/java/com/fourhorses/pitcherbe/user_account/controller/UserAccountController.java
@@ -3,11 +3,11 @@ package com.fourhorses.pitcherbe.user_account.controller;
 import com.fourhorses.pitcherbe.user_account.dto.UserAccountDto;
 import com.fourhorses.pitcherbe.user_account.service.UserAccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,5 +18,23 @@ public class UserAccountController {
     @PostMapping
     public ResponseEntity<UserAccountDto> register(@RequestBody UserAccountDto userAccountDto) {
         return ResponseEntity.ok(userAccountService.register(userAccountDto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserAccountDto> getUserAccount(@PathVariable Long id) {
+        return ResponseEntity.ok(userAccountService.getUserAccountById(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UserAccountDto>> getAllUserAccount() {
+        var userAccounts = userAccountService.getAllUserAccount();
+
+        HttpStatus status = userAccounts.isEmpty() ? HttpStatus.NO_CONTENT : HttpStatus.OK;
+        return new ResponseEntity<>(userAccounts, status);
+    }
+
+    @PatchMapping()
+    public ResponseEntity<UserAccountDto> updateUserAccount(@RequestBody UserAccountDto userAccountDto) {
+        return ResponseEntity.ok(userAccountService.updateUserAccount(userAccountDto));
     }
 }
--- pitcher-be/src/main/java/com/fourhorses/pitcherbe/user_account/service/UserAccountServiceImpl.java
+++ pitcher-be/src/main/java/com/fourhorses/pitcherbe/user_account/service/UserAccountServiceImpl.java
@@ -7,10 +7,11 @@ import com.fourhorses.pitcherbe.user_account.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -40,5 +41,32 @@ public class UserAccountServiceImpl implements UserAccountService {
     public UserAccountEntity getUserAccountEntityById(Long id) {
         return userAccountRepository.findById(id)
                 .orElseThrow(() -> new BusinessException("User not found"));
+    }
+
+    @Override
+    public UserAccountDto getUserAccountById(Long id) {
+        var userAccount = getUserAccountEntityById(id);
+        return modelMapper.map(userAccount, UserAccountDto.class);
+    }
+
+    @Override
+    public List<UserAccountDto> getAllUserAccount() {
+        var userAccounts = userAccountRepository.findAll();
+        return modelMapper.map(userAccounts, new TypeToken<List<UserAccountDto>>() {
+        }.getType());
+    }
+
+    @Override
+    public UserAccountDto updateUserAccount(UserAccountDto userAccountDto) {
+        UserAccountEntity userAccountEntity = getUserAccountEntityById(userAccountDto.getId());
+        userAccountEntity.setFirstName(userAccountDto.getFirstName());
+        userAccountEntity.setLastName(userAccountDto.getLastName());
+        userAccountEntity.setAddress(userAccountDto.getAddress());
+        userAccountEntity.setPhoneNumber(userAccountDto.getPhoneNumber());
+        userAccountEntity.setEmail(userAccountDto.getEmail());
+        userAccountEntity.setPassword(userAccountDto.getPassword());
+
+        var savedUser = userAccountRepository.save(userAccountEntity);
+        return modelMapper.map(savedUser, UserAccountDto.class);
     }
 }
