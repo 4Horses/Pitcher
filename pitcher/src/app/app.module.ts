faf4a12db1b421f8dba5a376efabc5ba22486402
Dinu Ionut-Alin <dinui.alin@gmail.com>
2023-04-01 14:41:45 +0300

    Create Login Form

--- pitcher/src/app/app.module.ts
+++ pitcher/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './features/registration/components/login/login.component';
 import { RegisterComponent } from './features/registration/components/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { RegisterComponent } from './features/registration/components/register/r
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
