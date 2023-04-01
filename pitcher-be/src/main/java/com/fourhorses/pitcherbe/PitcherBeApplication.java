e00a04efafbfb254a64af5f710877d029b923d75
Arhip Florin <65599211+Florin9925@users.noreply.github.com>
2023-04-01 13:43:48 +0300

    Created spring boot project

---
+++ pitcher-be/src/main/java/com/fourhorses/pitcherbe/PitcherBeApplication.java
@@ -0,0 +1,13 @@
+package com.fourhorses.pitcherbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitcherBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitcherBeApplication.class, args);
+    }
+
+}
---
+++ pitcher-be/src/test/java/com/fourhorses/pitcherbe/PitcherBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourhorses.pitcherbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitcherBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
